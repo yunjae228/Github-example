0d83e4fa23c7e80a5a269f0ae4653481d21d9ddd
yunjae228 <yvkqzhs7@gmail.com>
2021-07-26 19:22:02 +0900

    반응형 toggle 버튼 (main.js)

---
+++ main.js
@@ -0,0 +1,17 @@
+( function (window, document) {
+    'use strict';
+
+
+    const $toggles = document.querySelectorAll('.toggle');
+    const $toggleBtn = document.getElementById('toggle-btn');
+
+    $toggleBtn.addEventListener('click', function () {
+        toggleElements();
+    });
+
+    function toggleElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.toggle('on');
+        });
+    }
+})(window, document)
